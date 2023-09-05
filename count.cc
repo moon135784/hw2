ba56b975ed9085870ec4916a7f81946be820d8c9
moon135784 <moon135784@naver.com>
2023-09-05 20:49:52 +0900

    add count.cc

--- count.cc
+++ count.cc
@@ -1,6 +1,11 @@
 #include "count.h"
+using namespace std;
 
 void count_number() {
     // lab2 //
     // do it yourself //
+	for(int i = 1; i <= 10; i++){
+		cout << i << endl;
+	}
+	cout << "2018314814" << endl;
 }
